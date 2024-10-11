0d02bacdffccbccee93b13190a9aabe967055621
James Conte <James@mld.com>
2024-10-11 12:29:25 -0600

    first commit part 4

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,15 +1,15 @@
 import React from "react";
 import styles from './Cards.module.css';
 
-function Cards (){
+function Cards() {
     return (
         <div className={styles.titleContainer}>
             <h3>My Latest Work</h3>
-            <h2>I'm looking to expand my portfolio while I'm on top and while I'm young </h2>
+            <h2>I&apos;m looking to expand my portfolio while I&apos;m on top and while I&apos;m young</h2>
             <div className={styles.cardsContainer}>
                 <div className={styles.card}>
                     <img src="/images/mld.png" alt="card image" className={styles.cardImage} />
-                    <div class={styles.text}>
+                    <div className={styles.text}>
                         <h4>MLD.COM</h4>
                         <p>MLD.COM is a comprehensive website I developed for a luxury showroom specializing in high-end appliances, plumbing, and hardware. 
                             The site features a dynamic and responsive design, integrating a custom CMS to empower the content team with seamless control over product displays, 
@@ -21,4 +21,5 @@ function Cards (){
         </div>
     )
 }
-export default Cards; 
+
+export default Cards;
