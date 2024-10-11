83f1939a9588507b0537ddd5da0aa6e84cb532ef
James Conte <James@mld.com>
2024-10-11 12:52:27 -0600

    first commits part 6

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    reactStrictMode: true,    // Enables React strict mode
+    pageExtensions: ['js'],   // Recognizes .js files for routing
+    experimental: {
+      appDir: true,           // Enables the app directory for routing (Next.js 13+)
+    },
+  };
+  
+  export default nextConfig;
+  
